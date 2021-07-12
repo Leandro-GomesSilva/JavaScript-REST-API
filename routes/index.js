74f45290da63b900f6984ed06428d3ec6791c5a4
Le-Gomes <69648505+Le-Gomes@users.noreply.github.com>
2021-07-12 22:08:56 +0200

    Server Side Validation
    Adding server side validation for the routes /api/users POST, /api/courses POST and /api/courses/:id PUT

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 /*******************
  * 
- *  User Routes 
+ *  USER Routes 
  * 
  *******************/
 
@@ -21,20 +21,30 @@ router.get('/users', async (req, res) => {
 
 // Create a new user
 router.post('/users', async (req, res) => {
-  try{
+  const errors = [];    // Defining Array where errors will be stored
+  
+  // Validating firstName, lastName, emailAddress and password with ternary operators
+  !req.body.firstName ? errors.push('Please provide a value for "firstName"') : null;
+  !req.body.lastName ? errors.push('Please provide a value for "lastName"') : null;
+  !req.body.emailAddress ? errors.push('Please provide a value for "e-mail address"') : null;
+  !req.body.password ? errors.push('Please provide a value for "password"') : null;
+  
+  if (errors.length > 0) {
+    res
+      .status(400)
+      .json({ errors });
+  } else {
     await User.create(req.body);
     res
       .location('/')
       .status(201)
       .end();
-  } catch (error) {
-
   }
 });
 
 /*******************
  * 
- *  Course Routes 
+ *  COURSE Routes 
  * 
  *******************/
 
@@ -56,28 +66,44 @@ router.get('/courses/:id', async (req, res) => {
 
 // Create a new course
 router.post('/courses', async (req, res) => {
-  try{
+  const errors = [];    // Defining Array where errors will be stored
+
+  // Validating title and description with ternary operators
+  !req.body.title ? errors.push('Please provide a value for "title"') : null;
+  !req.body.description ? errors.push('Please provide a value for "description"') : null;
+
+  if (errors.length > 0) {
+    res
+      .status(400)
+      .json({ errors });
+  } else {
     const course = await Course.build(req.body);
     await course.save();
     res
       .location('/courses/' + course.id)
       .status(201)
       .end();
-  } catch (error) {
-
   }
 });
 
 // Update course with an ID ":id"
 router.put('/courses/:id', async (req, res) => {
-  try{
+  const errors = [];    // Defining Array where errors will be stored
+
+  // Validating title and description with ternary operators
+  !req.body.title ? errors.push('Please provide a value for "title"') : null;
+  !req.body.description ? errors.push('Please provide a value for "description"') : null;
+
+  if (errors.length > 0) {
+    res
+      .status(400)
+      .json({ errors });
+  } else {
     const course = await Course.findByPk(req.params.id);
     await course.update(req.body);
     res
       .status(204)
       .end();
-  } catch (error) {
-
   }
 });
 
